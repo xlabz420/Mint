d61c8015bbda01c9ce8b50d2c9ebc51fe84bf128
xlabz420 <131508089+xlabz420@users.noreply.github.com>
2023-04-25 08:18:04 +1000

    team

--- components/GitHubLink.tsx
+++ components/GitHubLink.tsx
@@ -5,12 +5,44 @@ export default function ThirdwebGuideFooter() {
   const url = "https://github.com/thirdweb-example/nft-drop";
 
   return (
-    
-    
     <>
       <div
         style={{
-         
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "40px",
+        }}
+      >
+        <div style={{ textAlign: "center" }}>
+          <img
+            src="/0.png"
+            alt="Team Member 1"
+            style={{ width: "150px", height: "150px", borderRadius: "50%" }}
+          />
+          <p style={{ color: "white"}}>Jose` Pepe (artist)</p>
+        </div>
+        <div style={{ textAlign: "center", marginLeft: "40px", marginRight: "40px" }}>
+          <img
+            src="/1.png"
+            alt="Team Member 2"
+            style={{ width: "150px", height: "150px", borderRadius: "50%" }}
+          />
+          <p style={{ color: "white"}}>Peperoni (manager)</p>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          <img
+            src="/7.png"
+            alt="Team Member 3"
+            style={{ width: "150px", height: "150px", borderRadius: "50%" }}
+          />
+          <p style={{ color: "white"}}>Mr.Pepe (brains)</p>
+        </div>
+      </div>
+
+      <div
+        style={{
+          textAlign: "center",
+          marginTop: "20px",
         }}
         role="button"
         onClick={() => window.open(url, "_blank")}
@@ -18,11 +50,11 @@ export default function ThirdwebGuideFooter() {
 
       <div
         style={{
-       
+          textAlign: "center",
+          marginTop: "10px",
         }}
       >
         <a href={url} target="_blank" rel="noopener noreferrer">
-         
           
         </a>
       </div>
